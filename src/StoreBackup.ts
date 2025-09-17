d6c45b0cb3f6c73d5c18498efde78b333181a850
Ralph Wiedemeier <ralph@framefactory.ch>
2025-09-17 16:48:29 +0200

    check for empty store, fix copy bug

--- src/StoreBackup.ts
+++ src/StoreBackup.ts
@@ -16,17 +16,25 @@ export class StoreBackup
 {
     /**
      * Copies all files from the source file store to the target file store.
+     * The target store must be empty, otherwise an exception is thrown.
      * @param source The source file store.
      * @param target The target file store.
      * @param prefix Optional prefix to filter files in the source store.
      */
     static async copyStore(source: FileStore, target: FileStore, prefix = ""): Promise<void>
     {
-        return source.list(prefix).then(async (fileNames) => {
-            for (const fileName of fileNames) {
-                const stream = await source.readStream(fileName);
-                await target.write(fileName, stream);
-            }
-        });
+        const empty = await target.empty();
+        if (!empty) {
+            throw new Error("Target store is not empty");
+        }
+
+        const fileNames = await source.list(prefix);
+        console.log(`[StoreBackup] copying ${fileNames.length} files...`);
+
+        for (const fileName of fileNames) {
+            console.log(`[StoreBackup] copying ${fileName}`);
+            const stream = await source.readStream(fileName);
+            await target.write(fileName, stream);
+        };
     }
 }
